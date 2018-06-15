4011eca54db8bbc451e84efebf235e053d3370e3
Hugo Tessier <hugo.tessier.1@ens.etsmtl.ca>
2018-06-14 21:59:14 -0400

    Correlation calculated and tested.

--- functions_tp2.rb
+++ functions_tp2.rb
@@ -4,11 +4,18 @@ class Functions
     def initialize( csv_name )  
       # Instance variables  
       @datas = []   
-      @average = 0
-      @square_distance = []
-      @sum_distance = 0
-      @variance = 0
-      @deviation = 0
+      @sum_X_x_Y = 0
+      @sum_X = 0
+      @sum_Y = 0
+      @numerator = 0
+
+      @sum_X_square = 0
+      @sum_square_X = 0
+      @sum_Y_square = 0
+      @sum_square_Y = 0
+      @denominator = 0
+
+      @correlation = 0
 
       x = 0
       CSV.foreach( csv_name ) do |row|
@@ -20,13 +27,95 @@ class Functions
               fullstring = row[0]
              end 
    
-             @datas[x] = fullstring.split(";").map(&:to_f)
-             # puts "#{ x }  : #{ @datas[x] } "
+             @datas[x-1] = fullstring.split(";").map(&:to_f)
           end
           x += 1
       end
 
       @data_count = @datas.count
     end  
+
+    def set_sum_X_x_Y
+      sum = 0
+
+      @datas.each do |data|
+        sum += data[0] * data[1]
+      end  
+
+      @sum_X_x_Y = sum
+      
+    end
+
+    def set_sum_X
+      sum = 0
+
+      @datas.each do |data|
+        sum += data[0] 
+      end  
+      
+      @sum_X = sum
+    end  
+
+    def set_sum_Y
+      sum = 0
+
+      @datas.each do |data|
+        sum += data[1] 
+      end  
+
+      @sum_Y = sum
+    end  
+
+    def set_numerator
+      @numerator =  @data_count * @sum_X_x_Y - ( @sum_X * @sum_Y )
+    end   
+
+    def set_sum_x_square
+      sum = 0
+
+      @datas.each do |data|
+        sum += data[0] * data[0]
+      end  
+
+      @sum_X_square = sum
+    end 
+
+    def set_sum_square_x
+      sum = 0
+
+      @datas.each do |data|
+        sum += data[0]
+      end  
+
+      @sum_square_X = sum * sum 
+    end 
+
+    def set_sum_y_square
+      sum = 0
+
+      @datas.each do |data|
+        sum += data[1] * data[1]
+      end  
+
+      @sum_Y_square = sum
+    end 
+
+    def set_sum_square_y
+      sum = 0
+
+      @datas.each do |data|
+        sum += data[1]
+      end  
+
+      @sum_square_Y = sum * sum
+    end 
+
+    def set_denominator
+      @denominator = Math.sqrt( ( ( @data_count *  @sum_X_square ) - @sum_square_X ) * ( ( @data_count * @sum_Y_square ) - @sum_square_Y ) )
+    end
+
+    def set_correlation 
+      @correlation = @numerator / @denominator
+    end 
     
 end  
--- main_tp2.rb
+++ main_tp2.rb
@@ -6,3 +6,23 @@ require_relative 'functions_tp2'
 csv = "data/data_tp2.csv"
 
 o = Functions.new( csv )
+
+o.set_sum_X_x_Y()
+
+o.set_sum_X()
+
+o.set_sum_Y()
+
+o.set_numerator()
+
+o.set_sum_x_square()
+
+o.set_sum_square_x()
+
+o.set_sum_y_square()
+
+o.set_sum_square_y()
+
+o.set_denominator()
+
+o.set_correlation()
