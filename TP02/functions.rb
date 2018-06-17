bdf118cd2a9d4ede7f83180fc20900684fc20351
Hugo Tessier <hugo.tessier.1@ens.etsmtl.ca>
2018-06-17 09:42:34 -0400

    Ajout de l'interpretation a l'affichage

--- TP02/functions.rb
+++ TP02/functions.rb
@@ -151,4 +151,25 @@ class Functions
     def get_correlation_square
       return @correlation_square
     end
+
+    def get_interpretation
+      output = ""
+
+      case 
+      when @correlation >= 0 && @correlation < 0.2
+        output = "Nulle a Faible"
+      when @correlation >= 0.2 && @correlation < 0.4
+        output = "Faible a Moyenne"
+      when @correlation >= 0.4 && @correlation < 0.7
+        output = "Moyenne a Forte"
+      when @correlation >= 0.7 && @correlation < 0.9
+        output = "Forte a Tres forte"
+      when @correlation >= 0.9 && @correlation <= 1
+        output = "Tres forte a Parfaite"  
+      else
+        output = "Erreur d'interpretation"  
+      end
+
+      return output
+    end 
 end  
